ce6d28c514decd40da7d092fc5ddef690a12d873
kashan <kashan,aliq148@gmail.com>
2026-04-14 10:43:47 +0530

    made some changes in prediction pipeline

--- src/pipeline/predict_pipeline.py
+++ src/pipeline/predict_pipeline.py
@@ -9,6 +9,7 @@ from flask import request
 from src.constant import *
 from src.utils.main_utils import MainUtils
 from dataclasses import dataclass
+from typing import Any
 
 @dataclass
 class PredictionPipelineConfig:
@@ -20,7 +21,7 @@ class PredictionPipelineConfig:
 
 
 class PredictionPipeline:
-    def __init__(self,request: request):
+    def __init__(self,request: Any):
 
         self.request = request
         self.utils = MainUtils()
